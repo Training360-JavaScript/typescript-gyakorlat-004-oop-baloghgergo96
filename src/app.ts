ac1f22ceb55d6c5948dfc7c83fecab8ed7c1a940
baloghgergo96 <baloghgergo0825@gmail.com>
2022-01-06 18:21:43 +0100

    done

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 // Importáld be a HumanHero és TransformerHero osztályokat.
-
+import { HumanHero } from "./hero";
+import { TransformerHero } from "./transformer";
 
 /**
  * Állítsd be helyesen a változót!
@@ -8,7 +9,29 @@
  * Hozz létre benne 3 HumanHero példányt, szabadon választott adatokkal.
  * @var {HumanHero[]} humans
  */
-export const humans: any[] = [];
+export const humans: HumanHero[] = [
+    {
+        id: 1,
+        name: 'John',
+        sex: 'female',
+        age: 20,
+        health: 55
+    },
+    {
+        id: 2,
+        name: 'Jane',
+        sex: 'male',
+        age: 32,
+        health: 88
+    },
+    {
+        id: 3,
+        name: 'Jake',
+        sex: 'female',
+        age: 11,
+        health: 100
+    },
+];
 
 /**
  * Állítsd be helyesen a változót!
@@ -17,4 +40,28 @@ export const humans: any[] = [];
  * Hozz létre benne 3 TransformerHero példányt, szabadon választott adatokkal.
  * @var {TransformerHero[]} humans
  */
-export const transformers: any[] = [];
+export const transformers: TransformerHero[] = [
+    {
+    id: 1,
+    name: 'Yang',
+    wings: 2,
+    wheels: 3,
+    clan: 'Chinese'
+    },
+
+    {
+    id: 2,
+    name: 'Li',
+    wings: 1,
+    wheels: 4,
+    clan: 'Japanese'
+   },
+
+   {
+    id: 3,
+    name: 'Yu',
+    wings: 6,
+    wheels: 1,
+   clan: 'Korean'
+  },
+];
